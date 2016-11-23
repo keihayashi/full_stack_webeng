b694000fc7b19eded5e9041b8db1b787987b92f7
keihayashi <keilee210@gmail.com>
2016-11-22 19:07:25 -0800

    Add Cancel button. Check self.user of NewPost class.

--- multi-user-blog-kate/handlers/newpost.py
+++ multi-user-blog-kate/handlers/newpost.py
@@ -8,16 +8,22 @@ def blog_key(name = 'default'):
 
 class NewPost(BlogHandler):
     def get(self):
-        self.render("newpost.html")
+        if self.user:
+            self.render("newpost.html")
+        else:
+            self.redirect('/login')
 
     def post(self):
-        subject = self.request.get('subject')
-        content = self.request.get('content')
+        if self.user:
+            subject = self.request.get('subject')
+            content = self.request.get('content')
 
-        if subject and content:
-            p = Post(parent = blog_key(), subject = subject, content = content, author = self.user.key(), liked_num = 0)
-            p.put()
-            self.redirect('/blog/%s' % str(p.key().id()))
+            if subject and content:
+                p = Post(parent = blog_key(), subject = subject, content = content, author = self.user.key(), liked_num = 0)
+                p.put()
+                self.redirect('/blog/%s' % str(p.key().id()))
+            else:
+                error = "subject and content, please!"
+                self.render("newpost.html", subject=subject, content=content, error=error, edit = False)
         else:
-            error = "subject and content, please!"
-            self.render("newpost.html", subject=subject, content=content, error=error, edit = False)
+            self.redirect('/login')
