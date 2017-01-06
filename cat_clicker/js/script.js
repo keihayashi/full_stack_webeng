a683f2c7742f1b61ba3343ec4fce4813277e9b2b
keihayashi <keilee210@gmail.com>
2017-01-06 01:49:33 -0800

    Select from 5 cats

--- cat_clicker/js/script.js
+++ cat_clicker/js/script.js
@@ -1,11 +1,25 @@
-var count = 0;
+var count = [0,0,0,0,0];
+var name_list = ["Alice", "Bob", "Cathey", "Dick", "Edward"];
+var cur = 1;
 
 $(function() {
-  $('.cat-button').click(function(e) {
+  $('#submit-btn').click(function(e) {
+    // chenge the cat pic and value.
+    var num = $('input[name=cat-select]:checked').val();
+    console.log(num);
+    cur = num;
+    var picture = "img/cat" + num + ".jpg";
+    $(".cat-img").attr("src", picture);
+    $('#cat-name').text(name_list[num - 1]);
+    $('#click-num').text(count[num - 1]);
+    return false;
+  });
+
+  $('#cat-button').click(function(e) {
     //the element has been clicked... do stuff here
-    count += 1;
-    console.log(count);
-    $('#click-num').text('You clicked ' + count + ' times.');
+    count[cur - 1] += 1;
+    console.log(count[cur - 1]);
+    $('#click-num').text(count[cur - 1]);
     return false;
   });
 });
