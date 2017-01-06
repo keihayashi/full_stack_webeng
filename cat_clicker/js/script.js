072971f415a45e833e318fe4d94fa33fe4ddae1a
keihayashi <keilee210@gmail.com>
2017-01-05 18:26:13 -0800

    Two cats

--- cat_clicker/js/script.js
+++ cat_clicker/js/script.js
@@ -1,7 +1,7 @@
 var count = 0;
 
 $(function() {
-  $('#my-elem').click(function(e) {
+  $('.cat-button').click(function(e) {
     //the element has been clicked... do stuff here
     count += 1;
     console.log(count);
