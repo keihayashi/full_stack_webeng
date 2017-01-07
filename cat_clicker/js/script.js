8a6282b25c9c1319c435b3cd761c3b1871f94b4e
keihayashi <keilee210@gmail.com>
2017-01-06 19:06:26 -0800

    Refactor based on model view controller rule

--- cat_clicker/js/script.js
+++ cat_clicker/js/script.js
@@ -1,25 +1,66 @@
-var count = [0,0,0,0,0];
-var name_list = ["Alice", "Bob", "Cathey", "Dick", "Edward"];
-var cur = 1;
 
 $(function() {
-  $('#submit-btn').click(function(e) {
-    // chenge the cat pic and value.
-    var num = $('input[name=cat-select]:checked').val();
-    console.log(num);
-    cur = num;
-    var picture = "img/cat" + num + ".jpg";
-    $(".cat-img").attr("src", picture);
-    $('#cat-name').text(name_list[num - 1]);
-    $('#click-num').text(count[num - 1]);
-    return false;
-  });
+  var model = [
+    {
+      "name" : "Alice",
+      "click" : 0
+    }, {
+      "name" : "Bob",
+      "click" : 0
+    }, {
+      "name" : "Cathey",
+      "click" : 0
+    }, {
+      "name" : "Dick",
+      "click" : 0
+    }, {
+      "name" : "Edward",
+      "click" : 0
+    }
+  ];
 
-  $('#cat-button').click(function(e) {
-    //the element has been clicked... do stuff here
-    count[cur - 1] += 1;
-    console.log(count[cur - 1]);
-    $('#click-num').text(count[cur - 1]);
-    return false;
-  });
-});
+  var octopus = {
+    addClick: function(cur) {
+      model[cur - 1].click += 1;
+      view_click.render(cur);
+    },
+    selectCat: function(cur) {
+      view_select.render(cur);
+    },
+    init: function() {
+      view_select.init();
+      view_click.init();
+    }
+  };
+
+  var view_select = {
+    init: function() {
+      var selectBtn = $('#submit-btn');
+      selectBtn.click(function() {
+          var cur = $('input[name=cat-select]:checked').val();
+          octopus.selectCat(cur);
+      });
+    },
+    render: function(cur) {
+      var picture = "img/cat" + cur + ".jpg";
+      $(".cat-img").attr("src", picture);
+      $('#cat-name').text(model[cur - 1].name);
+      $('#click-num').text(model[cur - 1].click);
+    }
+  };
+
+  var view_click = {
+    init: function() {
+      var clickCat = $('#cat-button');
+      clickCat.click(function() {
+        var cur = $('input[name=cat-select]:checked').val();
+        octopus.addClick(cur);
+      });
+    },
+    render: function(cur) {
+      $('#click-num').text(model[cur - 1].click);
+    }
+  };
+
+  octopus.init();
+}());
